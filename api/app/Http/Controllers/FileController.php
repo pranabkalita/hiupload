cdc58e598e3b1d4b659d92b04baf5da4c303e96a
Pranab Kalita <pavankalita@gmail.com>
2021-07-21 11:16:11 +0530

    Setup and configure for Aws S3 Bucket

--- api/app/Http/Controllers/FileController.php
+++ api/app/Http/Controllers/FileController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\FileResource;
+use Aws\S3\PostObjectV4;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class FileController extends Controller
 {
@@ -15,5 +17,25 @@ class FileController extends Controller
     public function index(Request $request)
     {
         return FileResource::collection($request->user()->files);
+    }
+
+    public function signed(Request $request)
+    {
+        $filename = md5($request->name . microtime()) . '.' . $request->extension;
+
+        $object = new PostObjectV4(
+            Storage::disk('s3')->getAdapter()->getClient(),
+            config('filesystems.disks.s3.bucket'),
+            ['key' => 'files/' . $filename],
+            [
+                ['bucket' => config(('filesystems.disks.s3.bucket'))],
+                ['starts-with', '$key', 'files/']
+            ]
+        );
+
+        return response()->json([
+            'attributes' => $object->getFormAttributes(),
+            'additionalData' => $object->getFormInputs(),
+        ]);
     }
 }
